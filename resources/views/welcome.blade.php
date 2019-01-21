8ecba67d03601a6796ba1beaf149cb2b26655e93
gitgut0110 <jsimms0110@gmail.com>
2019-01-21 11:17:14 +0100

    Procfile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My First LaravelPage JSimms
                 </div>
 
                 <div class="links">
